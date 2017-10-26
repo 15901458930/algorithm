98b7237233b13d1b3ac75bd003ecbeb53ecc6d81
Administrator <15901458930@163.com>
2017-10-26 19:37:01 +0800

    呵呵达

---
+++ src/main/java/com/wangpeng/learn/sort/bubble/BubbleSort.java
@@ -0,0 +1,23 @@
+package com.wangpeng.learn.sort.bubble;
+/**
+ * 冒泡排序
+ *
+ * 算法原理：将相邻的两个数对比，看是否需要互换位置，从而将最大或最小的数冒泡到最左或最右
+ */
+public class BubbleSort {
+
+
+    public int[] sort(int[] source){
+
+        if(source.length==0){
+            return null;
+        }
+
+        for(int i=0;i<source.length;i++){
+
+        }
+
+
+        return source;
+    }
+}
